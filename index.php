ba22a1f96b7a9d01f366f6cedb5b2a88b7021462
Shaaswatan Raghu Prakash <167835779+Happy-Help@users.noreply.github.com>
2025-01-31 18:41:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$submitpressed = $_POST["submitpressed"] ?? null;
+$programstatus = $_POST["progstatus"] ?? "false";
+$codes = $_POST["custinput"];
+$file = nl2br(file_get_contents("scannedcodes.txt"));
+echo "User: " . exec("whoami") . "<br>";
+echo "Submit Pressed: " . htmlspecialchars($submitpressed) . "<br>";
+echo "Program Status: " . htmlspecialchars($programstatus) . "<br>";
+$descriptor_spec = [
+    0 => ["pipe", "r"],
+    1 => ["pipe", "w"],
+    2 => ["pipe", "w"],
+];
+proc_open("sudo ./mainprogram", $descriptor_spec, $pipes);
+exec("xdg-open scannedcodes.txt");
+if (!empty($codes)) {
+    $file = fopen("/var/www/html/knowncodes.txt", "w");
+    if ($file) {
+        fwrite($file, $codes);
+        fclose($file);
+        echo "Codes written to file.<br>";
+    } else {
+        echo "Failed to open file for writing.<br>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Title</title>
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+    <body>
+        <h1 class="title">LIMIT N SIP</h1>
+        <br />
+        <div class="maindiv">
+            <form action="" name="mainform" method="POST">
+                <input type="hidden" value="" name="submitpressed" />
+                <input type="hidden" value="" name="progstatus" />
+                <input type="button" value="Start Program" onclick="setstatus(true), funcsubmit()" name="start" />
+                <input type="button" value="Stop Program" onclick="setstatus(false), funcsubmit()" name="stop" /><br />
+                Custom accepted barcodes:<br />
+                <textarea name="custinput"></textarea><br />
+                You can select a file to replace the file of allowed codes here. Each code must be in 7 digits. <br />
+                <input type="file" name="knowncodes" /> <br />
+                <input type="button" name="restartbtn" value="Restart" />
+                <br />
+                <p>How long to dispense:</p>
+                <input type="text" name="dispensetime" /><br />
+                <input type="button" value="Submit" onclick="funcsubmit()" />
+                <input type="reset" value="Reset" />
+            </form>
+            <br />
+            <div class="progstatus">
+                <h3>Program status:</h3><h4 id="status"></h4>
+                <h3>Fountain status:</h3><h4 id="fountainstatus"></h4>
+                <h3>Barcode status:</h3><h4 id="barcodestatus"></h4>
+                <h3>Last action performed:</h3><h4 id="lastaction"></h4>
+                <h3>Last code scanned:</h3><h4 id="lastcode"></h4>
+                <h3>Amount of codes declined:</h3><h4 id="declined"></h4>
+                <h3>Amount of codes used:</h3><h4 id="codesused"></h4>
+            </div>
+        </div>
+        <script>
+            let status = false;
+            function funcsubmit() {
+                document.getElementsByName("submitpressed")[0].value = true;
+                document.forms["mainform"].submit();
+            }
+            function setstatus(statusvar) {
+                status = statusvar;
+                document.getElementsByName("progstatus")[0].value = status;
+            }
+            
+            
+            
+        </script>
+    </body>
+</html>
---
+++ processing.php
@@ -0,0 +1,11 @@
+<!doctype HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>You should not be seeing this!</title>
+    </head>
+    
+    <body>
+        Hello! You should not see this right now! Please, do not press the back arrow, please exit the tab.
+    </body>
+</html>
